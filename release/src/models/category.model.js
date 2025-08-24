5da6b2672878a374938b821f8283823a40e164df
Rajeevkumar69 <rk488296@gmail.com>
2025-08-24 18:42:16 +0530

    deleted relese

--- release/src/models/category.model.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import categorySchema from "../schemas/category-schema.js";
-
-const Category = mongoose.model("categories", categorySchema);
-
-export default Category;
--- release/src/models/product.model.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import productSchema from "../schemas/product-schema.js";
-
-const Product = mongoose.model("products", productSchema);
-
-export default Product;
